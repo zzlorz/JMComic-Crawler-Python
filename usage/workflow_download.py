e38c667f3b37e78b70f6e3f0de86b563ee25e49f
zhuzhenglei <zhuzheng.lei@163.com>
2025-10-23 17:16:43 +0800

    Update workflow_download.py
    eee

--- usage/workflow_download.py
+++ usage/workflow_download.py
@@ -3,11 +3,9 @@ from jmcomic.cl import JmcomicUI
 
 # 下方填入你要下载的本子的id，一行一个，每行的首尾可以有空白字符
 jm_albums = '''
-JM422866
-422866
-jm22866
-23333
-'''.strip().split('\n')
+
+
+'''
 
 # 单独下载章节
 jm_photos = '''
